12f1d38e569e8c4269269f0b0ee91eb3dc497764
Rubgrsch <rubgrsch@gmail.com>
2016-09-09 10:38:55 +0800

    utf8.lua: Removed debug code

--- EnhancedChatFilter/Libs/utf8.lua
+++ EnhancedChatFilter/Libs/utf8.lua
@@ -3,21 +3,12 @@ utf8replace from @Phanx @Pastamancer
 --]]
 
 local strbyte, strlen, strsub, type = string.byte, string.len, string.sub, type
-local utfdebug = nil
 -- returns the number of bytes used by the UTF-8 character at byte i in s
 -- also doubles as a UTF-8 character validator
 
 local function utf8charbytes(s, i)
 	-- argument defaults
 	i = i or 1
-
-	-- argument checking
-	if utfdebug and type(s) ~= "string" then
-		error("bad argument #1 to 'utf8charbytes' (string expected, got ".. type(s).. ")")
-	end
-	if utfdebug and type(i) ~= "number" then
-		error("bad argument #2 to 'utf8charbytes' (number expected, got ".. type(i).. ")")
-	end
 
 	local c = strbyte(s, i)
 
@@ -31,39 +22,12 @@ local function utf8charbytes(s, i)
 		-- UTF8-2
 		local c2 = strbyte(s, i + 1)
 
-		if utfdebug and not c2 then
-			error("UTF-8 string terminated early")
-		end
-
-		-- validate byte 2
-		if utfdebug and (c2 < 128 or c2 > 191) then
-			error("Invalid UTF-8 character")
-		end
-
 		return 2
 
 	elseif c >= 224 and c <= 239 then
 		-- UTF8-3
 		local c2 = strbyte(s, i + 1)
 		local c3 = strbyte(s, i + 2)
-
-		if utfdebug and (not c2 or not c3) then
-			error("UTF-8 string terminated early")
-		end
-
-		-- validate byte 2
-		if utfdebug and (c == 224 and (c2 < 160 or c2 > 191)) then
-			error("Invalid UTF-8 character")
-		elseif utfdebug and (c == 237 and (c2 < 128 or c2 > 159)) then
-			error("Invalid UTF-8 character")
-		elseif utfdebug and (c2 < 128 or c2 > 191) then
-			error("Invalid UTF-8 character")
-		end
-
-		-- validate byte 3
-		if utfdebug and (c3 < 128 or c3 > 191) then
-			error("Invalid UTF-8 character")
-		end
 
 		return 3
 
@@ -72,29 +36,6 @@ local function utf8charbytes(s, i)
 		local c2 = strbyte(s, i + 1)
 		local c3 = strbyte(s, i + 2)
 		local c4 = strbyte(s, i + 3)
-
-		if utfdebug and (not c2 or not c3 or not c4) then
-			error("UTF-8 string terminated early")
-		end
-
-		-- validate byte 2
-		if utfdebug and (c == 240 and (c2 < 144 or c2 > 191)) then
-			error("Invalid UTF-8 character")
-		elseif utfdebug and (c == 244 and (c2 < 128 or c2 > 143)) then
-			error("Invalid UTF-8 character")
-		elseif utfdebug and (c2 < 128 or c2 > 191) then
-			error("Invalid UTF-8 character")
-		end
-
-		-- validate byte 3
-		if utfdebug and (c3 < 128 or c3 > 191) then
-			error("Invalid UTF-8 character")
-		end
-
-		-- validate byte 4
-		if utfdebug and (c4 < 128 or c4 > 191) then
-			error("Invalid UTF-8 character")
-		end
 
 		return 4
 
